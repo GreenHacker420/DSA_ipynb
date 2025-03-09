d71dbbb67f3562be10fdb547f2e48622e8ccfb9d
Harsh Hirawat <106510913+GreenHacker420@users.noreply.github.com>
2025-03-09 12:38:51 +0530

    Remove majority element function implementation from 1.py

--- 1.py
+++ 1.py
@@ -23,26 +23,3 @@
 #         print()
 # arr = [1,2,3,4]
 # sum_subarrays(arr)
-
-
-
-"""Given an array nums of size n, return the majority element.
-
-The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
-
-"""
-
-
-def majorityElement(nums):
-    n = len(nums)
-    dic = {}
-    for i in nums:
-        if i in dic:
-            dic[i] += 1
-        else:
-            dic[i] = 1
-    for i in dic:
-        if dic[i] > n//2:
-            return i
-    return -1
-
